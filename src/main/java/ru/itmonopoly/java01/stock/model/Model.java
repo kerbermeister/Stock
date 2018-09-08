d9da9b4a6d1fb9af521d1d561eb455d8ad85258b
techt0nix <flatleywalker@yandex.ru>
2018-09-08 13:58:42 +0300

    added_add_button

--- src/main/java/ru/itmonopoly/java01/stock/model/Model.java
+++ src/main/java/ru/itmonopoly/java01/stock/model/Model.java
@@ -4,6 +4,37 @@ import javax.persistence.*;
 
 @Entity
 public class Model {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVendorCode() {
+        return vendorCode;
+    }
+
+    public void setVendorCode(String vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
 
     @Id
     @GeneratedValue
