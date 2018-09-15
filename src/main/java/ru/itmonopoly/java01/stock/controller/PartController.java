9f5836523a3992bdc43a443531915037972957f5
techt0nix <flatleywalker@yandex.ru>
2018-09-15 16:28:29 +0300

    add_part_changes

--- src/main/java/ru/itmonopoly/java01/stock/controller/PartController.java
+++ src/main/java/ru/itmonopoly/java01/stock/controller/PartController.java
@@ -3,10 +3,7 @@ package ru.itmonopoly.java01.stock.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.itmonopoly.java01.stock.repo.PartRepository;
 
 @Controller
@@ -20,5 +17,16 @@ public class PartController {
         model.addAttribute("id", id);
         model2.addAttribute("parts", partRepository.findAll());
         return "part/parts_list";
+    }
+
+    @RequestMapping("/add_new")
+    public String requestAddNewPage(@PathVariable ("id") Long id, Model model) {
+        model.addAttribute("id", id);
+        return "part/add_new";
+    }
+
+    @PostMapping("/add_new")
+    public String addNewPart(@PathVariable ("id") Long id, @RequestParam ("name") String name, @RequestParam ("spec") String spec) {
+        return "redirect:/types";
     }
 }
